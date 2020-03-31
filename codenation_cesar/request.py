2c14959a506926829ee8d88f6b6af88969022e5b
Leonardo Celso de Souza <leonardo_souzac@hotmail.com>
2020-03-31 20:22:56 -0300

    Update request.py

--- codenation_cesar/request.py
+++ codenation_cesar/request.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-url = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=bb51878d4210953f4059c0b7a72693bfb7dfaced"
+url = "https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=seu-token"
 
 answer = {'answer': open(r'C:\Users\Leonardo\Desktop\codenation_cesar\answer.json', 'rb')}
 response = requests.post(url=url, files=answer)
